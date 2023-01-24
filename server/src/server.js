81884b0628e3a93a444bb809c40014bec40389ef
Sardor <abdullaevss94@mail.ru>
2023-01-24 16:19:22 +0500

    test

--- server/src/server.js
+++ server/src/server.js
@@ -1,25 +1,16 @@
 //DyYKwsRDUcun4uzt
 const http = require("http");
-const mongoose=require("mongoose")
 
 const PORT = process.env.PORT || 8000;
-const MONGO_URL='mongodb+srv://nasa-api:DyYKwsRDUcun4uzt@nasacluster.fsq4b2w.mongodb.net/nasa?retryWrites=true&w=majority';
-
 const app = require("./app");
+
+const { mongoConnect } = require("./services/mongo");
 const { loadPlanetsData } = require("./models/planets.model");
 
 const server = http.createServer(app);
 
-mongoose.connection.once('open',()=>{
-  console.log('MongoDB connection ready');
-})
-
-mongoose.connection.on("error",(err)=>{
-  console.error(err)
-})
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect();
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
@@ -27,11 +18,3 @@ async function startServer() {
 }
 
 startServer();
-
-// const express = require("express");
-// const app = express();
-// app.listen();
-
-// console.log(PORT);
-
-// "watch": "npm run server & npm run client",
