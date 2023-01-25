b6a17bbaf6b9474800c7eabd8579c45047faffac
Sardor <abdullaevss94@mail.ru>
2023-01-25 17:05:25 +0500

    Testing bug fix

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -25,14 +25,14 @@ describe("Launches API", () => {
     const completeLaunchData = {
       mission: "USS Enterprise",
       rocket: "NCC 1701-D",
-      target: "Kepler-186 f",
+      target: "Kepler-62 f",
       launchDate: "January 4, 2028",
     };
 
     const launchDataWithoutDate = {
       mission: "USS Enterprise",
       rocket: "NCC 1701-D",
-      target: "Kepler-186 f",
+      target: "Kepler-62 f",
     };
 
     const launchDataWithInvalidDate = {
@@ -42,7 +42,7 @@ describe("Launches API", () => {
       launchDate: "zoot",
     };
 
-    test("It should respond with 201 success", async () => {
+    test("It should respond with 201 created", async () => {
       const response = await request(app)
         .post("/launches")
         .send(completeLaunchData)
--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -14,11 +14,11 @@ async function mongoConnect() {
   await mongoose.connect(MONGO_URL);
 }
 
-function mongoDisconnect(){
-    await mongoose.disconnect();
+async function mongoDisconnect() {
+  await mongoose.disconnect();
 }
 
 module.exports = {
   mongoConnect,
-  mongoDisconnect
+  mongoDisconnect,
 };
